b08379d6b064c5eaf97d38f12677da0a81a05bc9
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-09-16 19:53:51 +0300

    added the env file

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,8 +3,21 @@ import './home.css'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Posts from '../../components/posts/Posts'
 import Header from '../../components/header/Header'
-
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import { useLocation } from "react-router";
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      console.log(res);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
           <Header/>
